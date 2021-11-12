6c21f06722ff07bc660833c389028da290183fb5
Serhii Shylo <serhii.shylo@mustpay.tech>
2021-11-12 12:56:20 +0200

    Add OcStore module

---
+++ for_ocstore_3.x/upload/catalog/language/en-gb/extension/payment/concordpay.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_title'] = '<span style="background: url(image/payment/concordpay.svg) 0 0 no-repeat !important;"><img src="image/payment/concordpay.png" alt="ConcordPay" title="ConcordPay"></span>&nbsp;Payment Visa, Mastercard, GooglePay, ApplePay';
+$_['text_payment_by_card'] = 'Payment by card on the site';
+?>
